ccbf86b41ccd4fa4b75ee867ec4f4299a816df0c
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2018-09-24 14:29:46 +0100

    Define additional ToText instances

--- src/Herd/Data/Text.hs
+++ src/Herd/Data/Text.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module Herd.Data.Text where
 
 import           Data.Text (Text)
@@ -5,6 +8,15 @@ import qualified Data.Text as T
 
 class ToText a where
   toText :: a -> Text
+
+instance ToText Text where
+  toText = id
+
+instance ToText Char where
+  toText = T.singleton
+
+instance ToText String where
+  toText = T.pack
 
 instance ToText Int where
   toText = T.pack . show
