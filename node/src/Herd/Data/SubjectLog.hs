59fceeb45ee2acc070e2f1eb7959fa9fda87a639
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2019-03-02 22:44:04 +0000

    Expose new data structure modules

--- node/src/Herd/Data/SubjectLog.hs
+++ node/src/Herd/Data/SubjectLog.hs
@@ -1,5 +1,5 @@
-module Herd.Data.RecordLog
-     ( RecordLog
+module Herd.Data.SubjectLog
+     ( SubjectLog
      , empty
      , subjects
      , addRecord
@@ -14,24 +14,24 @@ import           Data.Time.Clock     (UTCTime)
 
 import           Herd.Internal.Types
 
-data RecordLog = RecordLog (HashMap SubjectId [SubjectRecord]) Integer
+data SubjectLog = SubjectLog (HashMap SubjectId [SubjectRecord]) Integer
   deriving (Eq, Show, Read)
 
-empty :: RecordLog
-empty = RecordLog Map.empty 0
+empty :: SubjectLog
+empty = SubjectLog Map.empty 0
 
-subjects :: RecordLog -> [SubjectId]
-subjects (RecordLog index _) = Map.keys index
+subjects :: SubjectLog -> [SubjectId]
+subjects (SubjectLog index _) = Map.keys index
 
-addRecord :: SubjectId -> ByteString -> UTCTime -> RecordLog -> (SubjectRecord, RecordLog)
-addRecord subjectId payload time (RecordLog index lastSeqNum) =
+addRecord :: SubjectId -> ByteString -> UTCTime -> SubjectLog -> (SubjectRecord, SubjectLog)
+addRecord subjectId payload time (SubjectLog index lastSeqNum) =
   let newSeqNum = lastSeqNum + 1
       recordId  = SubjectRecordId subjectId newSeqNum
       record    = SubjectRecord recordId payload time
       newIndex  = Map.alter (Just . maybe [record] ((:) record)) subjectId index
-      newLog    = RecordLog newIndex newSeqNum
+      newLog    = SubjectLog newIndex newSeqNum
   in (record, newLog)
 
-getRecords :: SubjectId -> RecordLog -> [SubjectRecord]
-getRecords subjectId (RecordLog index _) =
+getRecords :: SubjectId -> SubjectLog -> [SubjectRecord]
+getRecords subjectId (SubjectLog index _) =
   maybe [] id $ Map.lookup subjectId index
