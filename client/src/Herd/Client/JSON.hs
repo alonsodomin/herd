864176774944715ef5de51d2f0a98b17729652a7
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2019-04-10 12:54:50 +0100

    Make herd client a newtype

--- client/src/Herd/Client/JSON.hs
+++ client/src/Herd/Client/JSON.hs
@@ -1,11 +1,16 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Herd.Client.JSON where
+module Herd.Client.JSON
+     ( ClientT
+     , runClient
+     ) where
 
 import           Conduit
 import           Control.Lens           ((^?))
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger
+import           Control.Monad.Reader
 import           Data.Conduit.Network   (clientSettings)
 import           Data.Text              (Text)
 import qualified Data.Text.Encoding     as T
@@ -14,10 +19,11 @@ import           Network.JSONRPC
 import           Herd.Client.Class
 import           Herd.Protocol
 
-type ClientT m = JSONRPCT m
+newtype ClientT m a = ClientT { unwrapClient :: JSONRPCT m a }
+  deriving (Functor, Applicative, Monad, MonadIO, MonadTrans, MonadLogger)
 
 instance MonadLoggerIO m => MonadClient (ClientT m) where
-  sendToServer req l = do
+  sendToServer req l = ClientT $ do
     rawRes <- sendRequest req
     res    <- foldResponse rawRes
 
@@ -37,4 +43,4 @@ runClient :: (MonadUnliftIO m, MonadLoggerIO m) => Text -> Int -> ClientT m a ->
 runClient host port action = do
   --let jsonrcpSettings = clientSettings (settings ^. csPort) (settings ^. csHost)
   let cs = clientSettings port (T.encodeUtf8 host)
-  jsonrpcTCPClient V2 True cs action
+  jsonrpcTCPClient V2 True cs (unwrapClient action)
