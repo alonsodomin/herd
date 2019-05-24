58e788d55f38a6235e806ec3229641e2dd5862e4
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2019-05-24 17:21:59 +0100

    Basic definition for a block

--- storage/src/Herd/Storage/Types.hs
+++ storage/src/Herd/Storage/Types.hs
@@ -1,3 +1,28 @@
 module Herd.Storage.Types where
 
-data Block = Block
+import           Control.Lens
+import           Data.String
+import           Data.ByteString   (ByteString)
+import           Data.Text    (Text)
+import qualified Data.Text    as T
+
+newtype BlockId = BlockId Text
+  deriving (Eq, Show)
+
+instance IsString BlockId where
+  fromString = BlockId . T.pack
+
+data Block =
+    Genesis BlockId
+  | Block BlockId BlockId ByteString
+  deriving (Eq, Show)
+
+blockId :: Getter Block BlockId
+blockId = to getBlockId
+  where getBlockId (Genesis bid)   = bid
+        getBlockId (Block bid _ _) = bid
+
+parentBlockId :: Getter Block (Maybe BlockId)
+parentBlockId = to getParentBlockId
+  where getParentBlockId (Genesis _)     = Nothing
+        getParentBlockId (Block _ pid _) = Just pid
