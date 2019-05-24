879114bfa79bf8dc4a1d824110e94706d713a6b8
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2019-05-25 01:07:56 +0200

    pre-loading of schemas from the console

--- console/src/Herd/Console/SchemaList.elm
+++ console/src/Herd/Console/SchemaList.elm
@@ -2,7 +2,8 @@ module Herd.Console.SchemaList exposing (SchemaList, main, view)
 
 import Browser
 import Dict exposing (Dict)
-import Herd.Console.Remote exposing (..)
+import Http
+import Herd.Console.Remote as Remote exposing (..)
 import Html exposing (Html, table, tbody, td, text, th, thead, tr)
 
 
@@ -18,30 +19,57 @@ main =
 type alias SchemaList =
     Dict SubjectId (List Version)
 
-
 type Msg
-    = GotSchemaList SchemaList
+    = GotSubjectIds (Result Http.Error (List SubjectId))
+    | GotSchemaVersions SubjectId (Result Http.Error (List Version))
 
+loadSubjectIds : Cmd Msg
+loadSubjectIds = Http.send GotSubjectIds <| Remote.getSubjects
+
+loadVersions : SubjectId -> Cmd Msg
+loadVersions subjectId = Http.send (GotSchemaVersions subjectId) <| Remote.getSubjectsBySubjectId subjectId
 
 type Model
     = Initializing
+    | Failure String
     | Ready SchemaList
 
+modelToSchemaList : Model -> SchemaList
+modelToSchemaList model =
+    case model of
+        Ready list -> list
+        _          -> Dict.empty
+
+handleError : Http.Error -> Model
+handleError err =
+    case err of
+        Http.BadUrl url -> Failure ("Bad url: " ++ url)
+        Http.Timeout -> Failure "Timed out!"
+        Http.NetworkError -> Failure "Network error!"
+        Http.BadStatus res -> Failure ("Bad status code: " ++ res.status.message ++ " url: " ++ res.url)
+        Http.BadPayload _ _ -> Failure "Bad payload"
 
 init : () -> ( Model, Cmd Msg )
-init _ =
-    let
-        loadSchemaList =
-            Cmd.none
-    in
-    ( Initializing, loadSchemaList )
-
+init _ = ( Initializing, loadSubjectIds )
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotSchemaList list ->
-            ( Ready list, Cmd.none )
+        GotSubjectIds result ->
+            case result of
+                Ok [] -> (Ready Dict.empty, Cmd.none)
+                Ok subjectIds ->
+                    (model, Cmd.batch (List.map loadVersions subjectIds))
+
+                Err err -> (handleError err, Cmd.none)
+
+        GotSchemaVersions subjectId result ->
+            case result of
+                Ok versions ->
+                    let list = modelToSchemaList model
+                    in (Ready (Dict.insert subjectId versions list), Cmd.none)
+                
+                Err err -> (handleError err, Cmd.none)
 
 
 subscriptions : Model -> Sub Msg
@@ -57,7 +85,9 @@ view : Model -> Html msg
 view model =
     case model of
         Initializing ->
-            text ""
+            text "Loading..."
+
+        Failure m -> text m
 
         Ready ls ->
             viewSchemaList ls
