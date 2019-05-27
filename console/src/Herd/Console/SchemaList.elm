397b52f0da99fe5d9a80a797497a77efa7f7405a
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2019-05-27 12:10:02 +0200

    formatting

--- console/src/Herd/Console/SchemaList.elm
+++ console/src/Herd/Console/SchemaList.elm
@@ -2,9 +2,9 @@ module Herd.Console.SchemaList exposing (SchemaList, main, view)
 
 import Browser
 import Dict exposing (Dict)
-import Http
 import Herd.Console.Remote as Remote exposing (..)
 import Html exposing (Html, table, tbody, td, text, th, thead, tr)
+import Http
 
 
 main =
@@ -19,57 +19,87 @@ main =
 type alias SchemaList =
     Dict SubjectId (List Version)
 
+
 type Msg
     = GotSubjectIds (Result Http.Error (List SubjectId))
     | GotSchemaVersions SubjectId (Result Http.Error (List Version))
 
+
 loadSubjectIds : Cmd Msg
-loadSubjectIds = Http.send GotSubjectIds <| Remote.getSubjects
+loadSubjectIds =
+    Http.send GotSubjectIds <| Remote.getSubjects
+
 
 loadVersions : SubjectId -> Cmd Msg
-loadVersions subjectId = Http.send (GotSchemaVersions subjectId) <| Remote.getSubjectsBySubjectId subjectId
+loadVersions subjectId =
+    Http.send (GotSchemaVersions subjectId) <| Remote.getSubjectsBySubjectId subjectId
+
 
 type Model
     = Initializing
     | Failure String
     | Ready SchemaList
 
+
 modelToSchemaList : Model -> SchemaList
 modelToSchemaList model =
     case model of
-        Ready list -> list
-        _          -> Dict.empty
+        Ready list ->
+            list
+
+        _ ->
+            Dict.empty
+
 
 handleError : Http.Error -> Model
 handleError err =
     case err of
-        Http.BadUrl url -> Failure ("Bad url: " ++ url)
-        Http.Timeout -> Failure "Timed out!"
-        Http.NetworkError -> Failure "Network error!"
-        Http.BadStatus res -> Failure ("Bad status code: " ++ res.status.message ++ " url: " ++ res.url)
-        Http.BadPayload _ _ -> Failure "Bad payload"
+        Http.BadUrl url ->
+            Failure ("Bad url: " ++ url)
+
+        Http.Timeout ->
+            Failure "Timed out!"
+
+        Http.NetworkError ->
+            Failure "Network error!"
+
+        Http.BadStatus res ->
+            Failure ("Bad status code: " ++ res.status.message ++ " url: " ++ res.url)
+
+        Http.BadPayload _ _ ->
+            Failure "Bad payload"
+
 
 init : () -> ( Model, Cmd Msg )
-init _ = ( Initializing, loadSubjectIds )
+init _ =
+    ( Initializing, loadSubjectIds )
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotSubjectIds result ->
             case result of
-                Ok [] -> (Ready Dict.empty, Cmd.none)
-                Ok subjectIds ->
-                    (model, Cmd.batch (List.map loadVersions subjectIds))
+                Ok [] ->
+                    ( Ready Dict.empty, Cmd.none )
 
-                Err err -> (handleError err, Cmd.none)
+                Ok subjectIds ->
+                    ( model, Cmd.batch (List.map loadVersions subjectIds) )
+
+                Err err ->
+                    ( handleError err, Cmd.none )
 
         GotSchemaVersions subjectId result ->
             case result of
                 Ok versions ->
-                    let list = modelToSchemaList model
-                    in (Ready (Dict.insert subjectId versions list), Cmd.none)
-                
-                Err err -> (handleError err, Cmd.none)
+                    let
+                        list =
+                            modelToSchemaList model
+                    in
+                    ( Ready (Dict.insert subjectId versions list), Cmd.none )
+
+                Err err ->
+                    ( handleError err, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -87,7 +117,8 @@ view model =
         Initializing ->
             text "Loading..."
 
-        Failure m -> text m
+        Failure m ->
+            text m
 
         Ready ls ->
             viewSchemaList ls
