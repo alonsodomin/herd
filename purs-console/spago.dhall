a782c427f80c10f66d1cd19c89a6b31c8e679991
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2019-06-04 20:10:42 +0100

    Remove package-lock.json

--- purs-console/spago.dhall
+++ purs-console/spago.dhall
@@ -12,6 +12,7 @@ You can edit this file as you like.
     , "effect"
     , "gen"
     , "halogen"
+    , "profunctor-lenses"
     , "psci-support"
     , "quickcheck"
     , "servant-support"
--- purs-console/src/Herd/Console/Schema/List.purs
+++ purs-console/src/Herd/Console/Schema/List.purs
@@ -2,9 +2,9 @@ module Herd.Console.Schema.List where
 
 import Prelude
 
-import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (Maybe(..))
+import Data.Semigroup.Foldable (class Foldable1, maximum)
 import Data.Set (Set)
 import Data.Tuple (Tuple(..))
 import Halogen as H
@@ -12,9 +12,14 @@ import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 
+import Herd.Console.Remote (getSubjects, getSubjectsBySubjectId)
 import Herd.Types (SubjectId(..), Version(..))
 
-type State = Map SubjectId Version
+
+type State =
+  { loading :: Boolean
+  , schemas :: Map SubjectId Version
+  }
 
 data Query a =
   GetSubjects (Set SubjectId -> a)
@@ -34,11 +39,11 @@ ui =
 
   where
     initialState :: State
-    initialState = Map.empty
+    initialState = { loading: false, schemas: Map.empty }
 
     render :: State -> H.ComponentHTML Query
     render state =
-      HH.ul [] $ map renderItem $ Map.toUnfoldable state
+      HH.ul [] $ map renderItem $ Map.toUnfoldable state.schemas
       where renderItem (Tuple (SubjectId subjectId) (Version version)) =
               HH.li [] [ HH.text subjectId ]
 
@@ -46,4 +51,7 @@ ui =
     eval = case _ of
       GetSubjects reply -> do
         state <- H.get
-        pure (reply $ Map.keys state)
+        pure (reply $ Map.keys state.schemas)
+
+latestVersion :: Foldable1 f => f Version -> Version
+latestVersion = maximum
